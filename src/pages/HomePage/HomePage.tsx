f1993f3c8fe7d4cf86309c38816cb153e9646101
Corrie Graham <corriegraham@Corries-MacBook-Air.local>
2021-10-09 12:05:19 +0200

    refac: changed useEffect usage and function calls

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,9 @@
 import './HomePage.css';
-import React, { FC, useEffect, useState } from 'react';
+import React, {
+  FC,
+  useEffect,
+  useState,
+} from 'react';
 import Loader from 'react-loader-spinner';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSortAlphaDown, faSortAlphaUp } from '@fortawesome/free-solid-svg-icons';
@@ -11,42 +15,37 @@ import { ALL_PEOPLE, NEXT_PAGE } from '../../grapqhl/queries';
 import { CustomInput } from '../../components/Utils/CustomInput';
 
 export const HomePage: FC = () => {
-  const [pageNumber, setPageNumber] = useState<number>(1);
-  const [initialLoadCompleted, setInitialLoadCompleted] = useState<boolean>(false);
-  const [isLoadingMore, setIsLoadingMore] = useState<boolean>(false);
   const [isSorted, setIsSorted] = useState<boolean>(false);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [pageRequested, setPageRequested] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<string>('');
+  const [requestedPage, setRequestedPage] = useState<string>('');
   const people = useStoreState((state) => state.people);
   const addPeopleToState = useStoreActions((actions) => actions.loadPeople);
   const loadRequestedPage = useStoreActions((actions) => actions.resetOrLoadPage);
   const { sortDesc } = useStoreState((state) => state);
   const { sortAsc } = useStoreState((state) => state);
   const { loading: isLoading, data } = useQuery(ALL_PEOPLE);
-  const [next, { loading: nextIsLoading, data: nextData }] = useLazyQuery(NEXT_PAGE);
+  const [next, { loading: nextPageIsLoading, data: nextData }] = useLazyQuery(NEXT_PAGE);
+  const [requestPage, { loading: requestedPageIsLoading, data: requestedPageData }] = useLazyQuery(NEXT_PAGE);
 
   useEffect(() => {
-    if (pageRequested && nextData) {
-      loadRequestedPage(nextData.next);
-      setPageRequested(false);
-    } else if ((data && !initialLoadCompleted) || nextData) {
-      const peopleToAddToState = isLoadingMore ? nextData.next : data.getAllPeople;
-      addPeopleToState(peopleToAddToState);
-      setInitialLoadCompleted(true);
-    }
-  }, [data, nextData]);
+    if (data) loadRequestedPage(data.getAllPeople);
+  }, [data, loadRequestedPage]);
+
+  useEffect(() => {
+    if (nextData) addPeopleToState(nextData.next);
+  }, [nextData, addPeopleToState]);
+
+  useEffect(() => {
+    if (requestedPageData) loadRequestedPage(requestedPageData.next);
+  }, [requestedPageData, loadRequestedPage]);
 
   const handleNext = () => {
-    const newPage = pageNumber + 1;
+    const newPage = currentPage === '' ? 2 : parseInt(currentPage, 10) + 1;
     next({ variables: { pageNumber: newPage.toString() } });
-    setIsLoadingMore(true);
-    setPageNumber(newPage);
+    setCurrentPage(newPage.toString());
   };
 
-  const handlePageRequest = (page: any) => {
-    next({ variables: { pageNumber: page.toString() } });
-    setPageRequested(true);
-  };
+  const handlePageRequest = () => { requestPage({ variables: { pageNumber: requestedPage.toString() } }); };
 
   const handleSort = () => {
     if (!isSorted) {
@@ -58,6 +57,11 @@ export const HomePage: FC = () => {
     }
 
     setIsSorted(!isSorted);
+  };
+
+  const handlePageNumberReset = () => {
+    setRequestedPage('');
+    requestPage({ variables: { pageNumber: '1' } });
   };
 
   return (
@@ -72,10 +76,11 @@ export const HomePage: FC = () => {
         </button>
 
         <CustomInput
-          isLoading={ nextIsLoading }
-          onChange={ (e: any) => setCurrentPage(e.target.value) }
-          handleSubmit={ () => handlePageRequest(currentPage) }
-          value={ currentPage }
+          isLoading={ requestedPageIsLoading }
+          onChange={ (e: any) => setRequestedPage(e.target.value) }
+          handleSubmit={ handlePageRequest }
+          handleReset={ handlePageNumberReset }
+          value={ requestedPage }
           placeholder="Request page"
         />
       </div>
@@ -96,7 +101,7 @@ export const HomePage: FC = () => {
         (!isLoading && people.length > 1) && (
           <div className="load__cta">
             <button type="button" onClick={ handleNext }>
-              { !nextIsLoading ? 'Load More' : (<Loader type="ThreeDots" color="#fff" height={ 20 } width={ 15 } />) }
+              { !nextPageIsLoading ? 'Load More' : (<Loader type="ThreeDots" color="#fff" height={ 20 } width={ 15 } />) }
             </button>
           </div>
         )
